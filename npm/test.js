7cbffeb17ff3b35afa0c14265bca5cf82929c985
subasith <vishaka.subasith@1billiontech.com>
2022-07-21 18:32:45 +0530

    second

--- npm/test.js
+++ npm/test.js
@@ -1,1 +1,13 @@
-import _ from 'lodash'
+const _ = require('lodash')
+
+const arr = [
+    {id:1, number:1},
+    {id:2, number:2},
+    {id:3, number:3},
+    {id:4, number:4},
+    {id:5, number:5}
+]
+
+arr.map(item =>{
+    console.log(_.get(item,'number',10))
+})
